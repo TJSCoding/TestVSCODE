e3ae6768dc52e7c948089733e168891398b7083c
Schmelzle <tischme@emea.corpdir.net>
2019-01-14 20:51:26 +0100

    Test

--- Excep.py
+++ Excep.py
@@ -1,9 +1,12 @@
-def koordnation_setzen(x,y)
+def koordnation_setzen(x,y):
     if x < 0 or y < 0:
         raise(ValueError("Ungültige Koordination:",x,y))
     print("Koordinaten gesetzt")
 def main():
-    koordnation_setzen(10, -5)
-    print("Scheint geklappt zu haben!")
+    try:
+        koordnation_setzen(10,-5)
+        print("Scheint geklappt zu haben!")
+    except ValueError:
+        print("Fehler abgefangen")
 
 main()
