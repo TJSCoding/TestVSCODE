87f643d46ae434b30cdc3324593d61179f397877
Schmelzle <tischme@emea.corpdir.net>
2019-01-14 21:11:47 +0100

    h

---
+++ MultiException.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        eingabe = int(input("Eingabe: "))
+        ergebnis = 10/eingabe
+
+        print(ergebnis)
+        break
+    except (ValueError, ZeroDivisionError):
+        print("Fehler bei der Berechnung!")
+    
+
